5452f7b2818099878444e3b6bdb9bd97b154516e
Marko Leinikka <Luukuton@users.noreply.github.com>
2021-12-29 00:26:12 +0200

    fix: add missing __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Thanks to Damien Elmes, the Migaku Japanese Add-on borrows lightly from his project.
+# https://ankiweb.net/shared/info/3918629684
+#
+# Thanks to Asweigart, I use Pyperclip in this project
+# https://github.com/asweigart/pyperclip
+#
+# Thanks to 奈幾乃 for the historical converter code
+# http://mto.herokuapp.com/
+#
+# Thanks to Lucas & Migaku ltd for making the original Migaku Japanese Addon.
+
+from . import main, modelChanger

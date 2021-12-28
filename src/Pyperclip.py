73a1b19e0df98d5fcf0df2a1d69a241f61a42991
Marko Leinikka <mleinikka@gmail.com>
2021-12-29 00:08:51 +0200

    Version 2.0.0

--- src/Pyperclip.py
+++ src/Pyperclip.py
@@ -43,7 +43,7 @@ A malicious user could rename or add programs with these names, tricking
 Pyperclip into running them with whatever permissions the Python process has.
 
 """
-__version__ = '1.7.0'
+__version__ = '2.0.0'
 
 import contextlib
 import ctypes
